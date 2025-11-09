2af19e7c61ef24a1c791df898064ed429c05939e
Abdelghafaar Nashaat <abdelghafaarnashaat@gmail.com>
2025-11-09 15:39:56 +0200

    cart & order UI

--- Frontend/ketabi/src/app/features/dashboard/components/dashboard.component/dashboard.component.ts
+++ Frontend/ketabi/src/app/features/dashboard/components/dashboard.component/dashboard.component.ts
@@ -80,7 +80,7 @@ categories = [
     this.bookService.getBooksByCategory(category).subscribe({
       next: (response) => {
         if (response.status === 'success') {
-          this.books = response.data.books;
+         this.books = response.data.books || response.data;
         } else {
           this.error = response.message;
         }
