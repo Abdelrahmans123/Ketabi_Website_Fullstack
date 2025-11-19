82f8583b3ce98ae45f774e9a65d0ee5fbf0df5dd
Abdelghafaar Nashaat <abdelghafaarnashaat@gmail.com>
2025-11-19 03:35:19 +0200

    api base url

--- Frontend/ketabi/src/environments/environment.ts
+++ Frontend/ketabi/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'http://16.171.234.223/api',
   social: {
     googleClientId: '115519062591-dp8beqjtdlscn5j1i1vq7aen4palro4c.apps.googleusercontent.com',
     facebookAppId: '1981504632695551',
   },
-  stripePubKey: 'pk_test_51SA6w5HPCHq1d9QMsWhxZkmpdzox62NRMtlp4TRMtOrpn22yhTqjGFjGgNgUPHPEUirFojivISKz50fdq3gVVmYS00RirbIcYs' 
+  stripePubKey: 'pk_test_51SA6w5HPCHq1d9QMsWhxZkmpdzox62NRMtlp4TRMtOrpn22yhTqjGFjGgNgUPHPEUirFojivISKz50fdq3gVVmYS00RirbIcYs'
 };
