ed2a7dc7729e680b1aa6a552e7a619cabc602d96
Abdelrahman Salah <sabdelrahman110@gmail.com>
2026-04-04 23:48:26 +0200

    chore: update production apiBaseUrl to new server IP

--- Frontend/ketabi/src/environments/environment.prod.ts
+++ Frontend/ketabi/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://51.21.128.69/api',
+  apiBaseUrl: 'http://13.51.207.138/api',
   stripePubKey: 'pk_test_51SA6w5HPCHq1d9QMsWhxZkmpdzox62NRMtlp4TRMtOrpn22yhTqjGFjGgNgUPHPEUirFojivISKz50fdq3gVVmYS00RirbIcYs',
   social: {
     googleClientId: '115519062591-dp8beqjtdlscn5j1i1vq7aen4palro4c.apps.googleusercontent.com',
